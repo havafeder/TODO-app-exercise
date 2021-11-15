4b96ebff04da7c63fd50af4708bb5a6db17464fd
havafeder <94397782+havafeder@users.noreply.github.com>
2021-11-15 14:55:01 -0800

    Update JSTodosExercise.js

--- JSTodosExercise.js
+++ JSTodosExercise.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() { 
+	const todoList = document.querySelector('#todo-list');
 	const form = document.querySelector('form');
-const todoList = document.querySelector('#todo-list');
-const newLi = document.createElement('li');
 
-const todoItems = JSON.parse(localStorage.getItem('todos')) || [];
-for (let i = 0; i < todoItems.length; i++){
+	const todoItems = JSON.parse(localStorage.getItem('todos')) || [];
+for (let i = 0; i < todoItems.length; i++) {
+	let newLi = document.createElement('li');
+	let newButton = document.createElement('button');
+	newButton.innerText = 'X';
 	newLi.innerText = todoItems[i].todo;
+	newLi.append(newButton);
 	todoList.appendChild(newLi);
 }
+
 	form.addEventListener('submit', function (e) {
 		e.preventDefault();
 
@@ -15,33 +18,31 @@ for (let i = 0; i < todoItems.length; i++){
 		const newButton = document.createElement('button');
 		const checked = document.createElement('INPUT');
 		const newLi = document.createElement('li');
-	
+		const todoCopy = document.querySelector('#todo-copy');
+		
 		checked.setAttribute('type', 'checkbox');
 		newLi.innerText = newTodo.value;
+		todoCopy.append(newLi.innerText);
 		newButton.innerText = 'X';
 		newLi.append(newButton);
 		newLi.append(checked);
 		todoList.append(newLi);
-		todoItems.push({ done: //don't know });
+		todoItems.push({ todo: todoCopy.innerText, done: false});
 		localStorage.setItem('todos', JSON.stringify(todoItems));
 	});
 
-todoList.addEventListener('click', function (e) {
-	if (e.target.tagName === 'BUTTON') {
-		e.target.parentElement.remove();
-	}
-	if (e.target.tagName === 'INPUT') {
-		e.target.parentElement.classList.toggle('crossed');
-	}
-	for (let i = 0; i < todoItems.length; i++){
-		if (todoItems[i].todo === e.target.innerText) {
-			localStorage.setItem('todos', JSON.stringify(todoItems));
+	todoList.addEventListener('click', function (e) {
+		if (e.target.tagName === 'BUTTON') {
+			e.target.parentElement.remove();
 		}
-	}
-});
+		if (e.target.tagName === 'INPUT') {
+			e.target.parentElement.classList.toggle('crossed');
+		}
+		for (let i = 0; i < todoItems.length; i++) {
+			if (todoItems[i].todo === e.target.innerText) {
+				localStorage.setItem('todos', JSON.stringify(todoItems));
+			}
+		}
 	});
-
-
-
 
 

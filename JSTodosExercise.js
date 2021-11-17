3f1b2ac6d3b23d3c27a507b41623341a7eacaa62
havafeder <94397782+havafeder@users.noreply.github.com>
2021-11-16 17:52:23 -0800

    Update JSTodosExercise.js

--- JSTodosExercise.js
+++ JSTodosExercise.js
@@ -1,48 +1,49 @@
-const todoList = document.querySelector('#todo-list');
-const form = document.querySelector('form');
-const newTodo = document.createElement('li');
-const newButton = document.createElement('button');
-const newCheck = document.createElement('input');
-newCheck.type = "checkbox";
-
-const todoItems = JSON.parse(localStorage.getItem('todos')) || [];
-for (let i = 0; i < todoItems.length; i++) {
-	newTodo.innerText = todoItems[i].todo;
-	newButton.innerText = 'X';
-	newTodo.appendChild(newCheck);
-	todoList.appendChild(newTodo);
-	newTodo.appendChild(newButton);
-}
-
-form.addEventListener('submit', function (e) {
-	e.preventDefault();
-
-	const addTodo = document.querySelector('#new-todo');
-	const newTodo = document.createElement('li');
-	const newButton = document.createElement('button');
-	const newCheck = document.createElement('input');
-	newCheck.type = "checkbox";
-
-	newTodo.innerText = addTodo.value;
-	newButton.innerText = 'X';
-	newTodo.appendChild(newCheck);
-	todoList.appendChild(newTodo);
-	newTodo.appendChild(newButton);
-	todoItems.push({ todo: newTodo.innerText, done: false });
-	localStorage.setItem('todos', JSON.stringify(todoItems));
-});
-
-todoList.addEventListener('click', function (e) {
-	if (e.target.tagName === 'BUTTON') {
-		e.target.parentElement.remove();
-	}
-	else if (e.target.tagName === 'INPUT') {
-		e.target.parentElement.classList.toggle('crossed');
-	}
+document.addEventListener('DOMContentLoaded', function (e) {
+	const todoList = document.querySelector('#todo-list');
+	const form = document.querySelector('form');
+	
+	const todoItems = JSON.parse(localStorage.getItem('todos')) || [];
 	for (let i = 0; i < todoItems.length; i++) {
-		if (todoItems[i].todo === e.target.innerText) {
+		const newTodo = document.createElement('li');
+		const newButton = document.createElement('button');
+		const newCheck = document.createElement('input');
+		const todoText = document.createElement('p');
+		newCheck.type = "checkbox";
+		todoText.innerText = todoItems[i].todo;
+		newButton.innerText = 'X';
+		newTodo.appendChild(newCheck);
+		newTodo.appendChild(todoText);
+		todoList.appendChild(newTodo);
+		newTodo.appendChild(newButton);
+	};
+
+	form.addEventListener('submit', function (e) {
+		e.preventDefault();
+
+		const addTodo = document.querySelector('#new-todo');
+		const newTodo = document.createElement('li');
+		const newButton = document.createElement('button');
+		const newCheck = document.createElement('input');
+		const todoText = document.createElement('p');
+		newCheck.type = "checkbox";
+   
+		todoText.innerText = addTodo.value;
+		newButton.innerText = 'X';
+		newTodo.appendChild(newCheck);
+		newTodo.appendChild(todoText);
+		todoList.appendChild(newTodo);
+		newTodo.appendChild(newButton);
+		todoItems.push({todo: todoText.innerText, done: false})
+		localStorage.setItem('todos', JSON.stringify(todoItems));
+	});
+
+	todoList.addEventListener('click', function (e) {
+		if (e.target.tagName === 'BUTTON') {
+			e.target.parentElement.remove();
+		}
+		if (e.target.tagName === 'INPUT') {
+			e.target.parentElement.classList.toggle('crossed');
 			localStorage.setItem('todos', JSON.stringify(todoItems));
 		}
-	}
+	});
 });
-
